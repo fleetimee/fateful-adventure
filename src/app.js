0cc043b9cf143c3425a8ed71ec65057a4c20e7a6
febriaricandra <febriaricandraproduction@gmail.com>
2022-05-16 15:00:41 +0700

    add fetch

--- src/app.js
+++ src/app.js
@@ -4,3 +4,22 @@ const menu = document.querySelector(".mobile-menu");
 btn.addEventListener("click", () => {
   menu.classList.toggle("hidden");
 });
+
+fetch('./json/db.json')
+  .then(function(response){
+    return response.json();
+  })
+  .then(function(data){
+    appendData(data);
+  })
+  .catch(function(err){
+    console.log('error:' + err);
+  });
+  function appendData(data){
+    var products = document.getElementById("Products")
+    for (let i=0; i<data.length; i++){
+      var div = document.createElement("div")
+      div.innerHTML = 'id' + data[i].id + ' ' + data[i].title;
+      products.appendChild(div)
+    }
+  }
